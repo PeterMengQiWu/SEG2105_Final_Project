adf07bdb2f6baafbd9c2f78db9b1731d1c0201c1
YuanZheng <bhu078@uottawa.ca>
2017-11-23 15:54:48 -0500

    YHZ

--- app/src/main/java/ca/uottawa/leyaoli/seg2105_final_project/Tab1.java
+++ app/src/main/java/ca/uottawa/leyaoli/seg2105_final_project/Tab1.java
@@ -1,19 +1,28 @@
 package ca.uottawa.leyaoli.seg2105_final_project;
 
+import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+
 /**
- * Created by whtit on 2017-11-23.
+ * Created by yuanzheng on 2017/11/21.
  */
 
+
 public class Tab1 extends Fragment {
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout,container,false);
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_tab1,container,false);
         return view;
     }
+
+
 }
--- app/src/main/java/ca/uottawa/leyaoli/seg2105_final_project/Tab3.java
+++
@@ -1,8 +0,0 @@
-package ca.uottawa.leyaoli.seg2105_final_project;
-
-/**
- * Created by whtit on 2017-11-23.
- */
-
-public class Tab3 {
-}
