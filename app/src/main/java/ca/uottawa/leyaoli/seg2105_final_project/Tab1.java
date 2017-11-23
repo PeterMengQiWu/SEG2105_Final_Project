f2498daef548f3f3dd10cc77982912cd6f27159c
YuanZheng <bhu078@uottawa.ca>
2017-11-23 15:46:27 -0500

    YHZ

---
+++ app/src/main/java/ca/uottawa/leyaoli/seg2105_final_project/Tab1.java
@@ -0,0 +1,19 @@
+package ca.uottawa.leyaoli.seg2105_final_project;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by whtit on 2017-11-23.
+ */
+
+public class Tab1 extends Fragment {
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout,container,false);
+        return view;
+    }
+}
---
+++ app/src/main/java/ca/uottawa/leyaoli/seg2105_final_project/Tab3.java
@@ -0,0 +1,8 @@
+package ca.uottawa.leyaoli.seg2105_final_project;
+
+/**
+ * Created by whtit on 2017-11-23.
+ */
+
+public class Tab3 {
+}
