5363188d0ef6c8431900fb999ba9a0c6e531b232
YuanZheng <bhu078@uottawa.ca>
2017-11-24 01:07:28 -0500

    YHZ

--- app/src/main/java/ca/uottawa/leyaoli/seg2105_final_project/ChoreDetail.java
+++ app/src/main/java/ca/uottawa/leyaoli/seg2105_final_project/ChoreDetail.java
@@ -2,6 +2,7 @@ package ca.uottawa.leyaoli.seg2105_final_project;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class ChoreDetail extends AppCompatActivity {
 
@@ -9,5 +10,17 @@ public class ChoreDetail extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chore_detail);
+
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+    public  boolean onOptionsItemSelected (MenuItem item){
+        int id = item.getItemId();
+
+        if (id == android.R.id.home){
+            this.finish();
+        }
+
+        return  super.onOptionsItemSelected(item);
     }
 }
