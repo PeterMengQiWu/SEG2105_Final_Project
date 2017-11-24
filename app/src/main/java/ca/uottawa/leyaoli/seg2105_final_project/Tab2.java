05ac2bcc6458bf5af73449f5f0f6d508b8851841
YuanZheng <bhu078@uottawa.ca>
2017-11-24 00:41:06 -0500

    YHZ

--- app/src/main/java/ca/uottawa/leyaoli/seg2105_final_project/Tab2.java
+++ app/src/main/java/ca/uottawa/leyaoli/seg2105_final_project/Tab2.java
@@ -16,8 +16,8 @@ public class Tab2 extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-        return inflater.inflate(R.layout.fragment_tab2, container, false);
+        View view = inflater.inflate(R.layout.fragment_tab2,container,false);
+        return view;
     }
 
 
