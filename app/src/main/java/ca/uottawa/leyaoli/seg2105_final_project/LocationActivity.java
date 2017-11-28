a26a4c987ac5710b0e840c77c7895308c4ca4525
kevin9804 <lileyao1998@gmail.com>
2017-11-28 09:28:11 -0500

    Navigate to matket address

--- app/src/main/java/ca/uottawa/leyaoli/seg2105_final_project/LocationActivity.java
+++ app/src/main/java/ca/uottawa/leyaoli/seg2105_final_project/LocationActivity.java
@@ -6,19 +6,74 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class LocationActivity extends AppCompatActivity {
+
+    private RadioGroup martket;
+    private RadioButton walmart;
+    private RadioButton loblaws;
+    private RadioButton metro;
+    private RadioButton CT;
+    private RadioButton bestbuy;
+    private RadioButton TnT;
+    private RadioButton shoppers;
+    private RadioButton other;
+    EditText marketAddress;
+    String address;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location);
+
+        martket = (RadioGroup)findViewById(R.id.market);
+        walmart = (RadioButton)findViewById(R.id.walmart);
+        loblaws = (RadioButton)findViewById(R.id.loblaw);
+        metro = (RadioButton)findViewById(R.id.metro);
+        CT = (RadioButton)findViewById(R.id.canadian_tire);
+        bestbuy = (RadioButton)findViewById(R.id.bestbuy);
+        TnT = (RadioButton)findViewById(R.id.T_n_T);
+        shoppers = (RadioButton)findViewById(R.id.shoppers_drug_mart);
+        other = (RadioButton)findViewById(R.id.other);
+
+        martket.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if (checkedId == walmart.getId()){
+                    address = walmart.getText().toString();
+                }
+                else if (checkedId == loblaws.getId()){
+                    address = loblaws.getText().toString();
+                }
+                else if (checkedId == metro.getId()){
+                    address = metro.getText().toString();
+                }
+                else if (checkedId == CT.getId()){
+                    address = CT.getText().toString();
+                }
+                else if (checkedId == bestbuy.getId()){
+                    address = bestbuy.getText().toString();
+                }
+                else if (checkedId == TnT.getId()){
+                    address = TnT.getText().toString();
+                }
+                else if (checkedId == shoppers.getId()){
+                    address = shoppers.getText().toString();
+                }
+                else if (checkedId == other.getId()){
+                    marketAddress = (EditText)findViewById(R.id.editText);
+                    address = marketAddress.getText().toString();
+                }
+            }
+        });
     }
 
-    public void OnOpenInGoogleMaps (View view) {
-        EditText marketAddress = (EditText) findViewById(R.id.editText);
+    public void NavigateButton (View view) {
         // Create a Uri from an intent string. Use the result to create an Intent.
-        Uri gmmIntentUri = Uri.parse("http://maps.google.co.in/maps?q="+marketAddress.getText());
+        Uri gmmIntentUri = Uri.parse("http://maps.google.co.in/maps?q="+address);
         // Create an Intent from gmmIntentUri. Set the action to ACTION_VIEW
         Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
         // Make the Intent explicit by setting the Google Maps package
