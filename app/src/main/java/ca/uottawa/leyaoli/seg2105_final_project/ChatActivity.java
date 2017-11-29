b7b688ad875ee9322c6fd550868086522f00a21e
YuanZheng <bhu078@uottawa.ca>
2017-11-29 13:23:35 -0500

    YHZ

--- app/src/main/java/ca/uottawa/leyaoli/seg2105_final_project/ChatActivity.java
+++ app/src/main/java/ca/uottawa/leyaoli/seg2105_final_project/ChatActivity.java
@@ -4,14 +4,18 @@ import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -19,7 +23,10 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ServerValue;
 import com.google.firebase.database.ValueEventListener;
 
+import java.security.PrivateKey;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 
@@ -31,24 +38,42 @@ public class ChatActivity extends AppCompatActivity {
     private DatabaseReference userdatabase;
     private FirebaseAuth firebaseAuth;
     private String userID;
+    //==============================================================
+    private final List<Messgae> messgaeList = new ArrayList<>();
+    private LinearLayoutManager linearLayoutManager ;
+    private MessageAdapter messageAdapter;
+    private RecyclerView messageView ;
+
+// =============================================================================
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+// =================================================================================FireBase Varibaless
         userdatabase = FirebaseDatabase.getInstance().getReference();
         firebaseAuth = FirebaseAuth.getInstance();
         userID = firebaseAuth.getCurrentUser().getUid();
 
-        chatToolbar = (Toolbar)findViewById(R.id.chat_tool_bar);
-       setSupportActionBar(chatToolbar);
 
+
+// ====================================================== Get component in UI
+        chatToolbar = (Toolbar)findViewById(R.id.chat_tool_bar);
+        setSupportActionBar(chatToolbar);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
         chatSendBt = (ImageButton) findViewById(R.id.sendButton );
         chatMessage = (EditText) findViewById(R.id.chatTextview);
+        messageView = (RecyclerView) findViewById(R.id.chatView);
+// =================================================================================== adapter setup
+        messageAdapter = new MessageAdapter(messgaeList);
+
+        linearLayoutManager = new LinearLayoutManager(this);
 
 
+// ======================================================================================Recycle View setup
+
+//=====================================================================================Set up Chat Title
        chatUser = getIntent().getStringExtra("user_id");
         userdatabase.child("Users").child(chatUser).addValueEventListener(new ValueEventListener() {
             @Override
@@ -62,18 +87,19 @@ public class ChatActivity extends AppCompatActivity {
 
             }
         });
-
+//===================================================================================add  CHAT to database
         userdatabase.child("Chat").child(userID).addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot)
             {
-                if (!dataSnapshot.hasChild(userID)){
+                if (!dataSnapshot.hasChild(chatUser)){
                     Map chatAddmap = new HashMap();
                     chatAddmap.put("seen",false);
                     chatAddmap.put("timestamp", ServerValue.TIMESTAMP);
+
                     Map userMap =new HashMap();
-                    userMap.put("Chat" + userID +"/" + chatUser,chatAddmap);
-                    userMap.put( "Chat" + chatUser +"/" + userID,chatAddmap);
+                    userMap.put("Chat/" + userID +"/" + chatUser,chatAddmap);
+                    userMap.put( "Chat/" + chatUser +"/" + userID,chatAddmap);
 
                     userdatabase.updateChildren(userMap, new DatabaseReference.CompletionListener() {
                         @Override
@@ -95,7 +121,11 @@ public class ChatActivity extends AppCompatActivity {
             }
         });
 
-
+// ======================================================================================Recycle View setup
+        messageView.setHasFixedSize(true);
+        messageView.setLayoutManager(linearLayoutManager);
+        messageView.setAdapter(messageAdapter);
+        receiveMessage(); // receive data
 //=========================================================================================================message
 
 
@@ -110,16 +140,49 @@ public class ChatActivity extends AppCompatActivity {
 
 
     }
+// ======================================================================================= receivce
+    private void receiveMessage() {
 
+        userdatabase.child("message").child(userID).child(chatUser).addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                Messgae e = dataSnapshot.getValue(Messgae.class);
 
+                messgaeList.add(e);
+              messageAdapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+// ================================================================================================SEND MESSAGE
 
     private void sendMessage() {
         String message = chatMessage.getText().toString();
 
-        if (TextUtils.isEmpty(message)){
+        if (!TextUtils.isEmpty(message)){
 
-            String curren_user = "Message" + userID + "/" +chatUser;
-            String other_user = "Message" + chatUser+ "/" + userID;
+            String curren_user = "message/" + userID + "/" +chatUser;
+            String other_user = "message/" + chatUser+ "/" + userID;
 
             DatabaseReference user_message_push = userdatabase.child("Message").child(userID).child(chatUser).push();
             String push_id = user_message_push.getKey();
