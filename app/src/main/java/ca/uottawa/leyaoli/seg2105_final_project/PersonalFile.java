908a6fcfa8e125209403275639c95daae655432e
YuanZheng <bhu078@uottawa.ca>
2017-11-30 00:06:28 -0500

    YHZ

--- app/src/main/java/ca/uottawa/leyaoli/seg2105_final_project/PersonalFile.java
+++ app/src/main/java/ca/uottawa/leyaoli/seg2105_final_project/PersonalFile.java
@@ -1,8 +1,11 @@
 package ca.uottawa.leyaoli.seg2105_final_project;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -23,6 +26,7 @@ public class PersonalFile extends AppCompatActivity {
     private DatabaseReference userdatabase;
     private FirebaseAuth firebaseAuth;
     private Toolbar toolbar;
+    private Button bt ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +34,7 @@ public class PersonalFile extends AppCompatActivity {
         firebaseAuth = FirebaseAuth.getInstance();
         userdatabase = FirebaseDatabase.getInstance().getReference();
         toolbar = (Toolbar) findViewById(R.id.personalProfileToolbar);
+        bt = (Button) findViewById(R.id.btnSendMessage);
         setSupportActionBar(toolbar);
 
         // ===================================================================
@@ -46,6 +51,18 @@ public class PersonalFile extends AppCompatActivity {
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
+
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               Intent intent = new Intent(PersonalFile.this, ChatActivity.class);
+                intent.putExtra("user_id",chatUser);
+                startActivity(intent);
 
             }
         });
