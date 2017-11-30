7c699d7edab5509a00c3f8731702c75382f9a0ee
kevin9804 <lileyao1998@gmail.com>
2017-11-30 02:34:32 -0500

    Chore detail page

--- app/src/main/java/ca/uottawa/leyaoli/seg2105_final_project/ChoreDetail.java
+++ app/src/main/java/ca/uottawa/leyaoli/seg2105_final_project/ChoreDetail.java
@@ -1,29 +1,50 @@
 package ca.uottawa.leyaoli.seg2105_final_project;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
 /**
  * Created by yuanzheng on 2017/11/21.
  */
 
 public class ChoreDetail extends AppCompatActivity {
 
+    private String task_name;
+    private Task task;
+
+    private EditText taskName;
+    private TextView points;
+    private TextView dueDate;
+    private TextView dueTime;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chore_detail);
 
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-    }
-    public  boolean onOptionsItemSelected (MenuItem item){
-        int id = item.getItemId();
+        //getSupportActionBar().setDisplayShowHomeEnabled(true);
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        if (id == android.R.id.home){
-            this.finish();
+        taskName = (EditText)findViewById(R.id.taskName);
+        points = (TextView)findViewById(R.id.points);
+        dueDate = (TextView)findViewById(R.id.dueDate);
+        dueTime = (TextView)findViewById(R.id.dueTime);
+
+        task_name = getIntent().getStringExtra("task name");
+
+        TasksDBHandler db = new TasksDBHandler(this);
+        task = db.findTaskByName(task_name);
+        if(task!=null) {
+            taskName.setText(task_name);
+            points.setText((task.getPoints() + "").toString());
+            dueDate.setText(task.getDueDate().toString());
+            dueTime.setText(task.getDueTime().toString());
         }
-
-        return  super.onOptionsItemSelected(item);
     }
 }
